c9820788044919e488aa2510bbfe30b9d7fad5c7
Tyler Freckmann <tyler.freckmann@sas.com>
2018-06-01 15:47:30 -0400

    change default port app listens on to 7050

--- routes.py
+++ routes.py
@@ -58,4 +58,4 @@ def allowed_file(filename):
 		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0', port=7050)
