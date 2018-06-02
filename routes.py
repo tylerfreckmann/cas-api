f8e2eacf32608bf12a9200b270f5598b70317d08
Tyler Freckmann <tyler.freckmann@sas.com>
2018-06-01 23:13:10 -0400

    convert tabs to spaces

--- routes.py
+++ routes.py
@@ -18,46 +18,46 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-	if request.method == 'POST':
-		# check if the post request has the file part
-		if 'file' not in request.files:
-			print('No file part')
-			return redirect(url_for('index'))
-		file = request.files['file']
-		if file.filename == '':
-			print('No selected file')
-			return redirect(url_for('index'))
-		if file:
-			filename = secure_filename(file.filename)
-			filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-			file.save(filepath)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(url_for('index'))
+        file = request.files['file']
+        if file.filename == '':
+            print('No selected file')
+            return redirect(url_for('index'))
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
 
-			# CAS Image Processing
-			s = swat.CAS('localhost', 5570, authinfo=AUTHINFO)
-			s.loadactionset('image')
-			s.loadimages(filepath, casout={'name':'img'})
-			s.loadactionset('astore')
-			s.score(table={'name':'img'}, out={'name':'score'}, rstore={'name':ASTORE, 'caslib':ASTORE_LIB})
-			scores = s.fetch(table={'name':'score'})['Fetch'].loc[0,:].to_dict()
-			label = scores.pop('I__label_')
-			s.endSession()
-			return jsonify({'imgUrl': url_for('uploaded_file', filename=filename),
-				'label': label,
-				'scores': scores})
+            # CAS Image Processing
+            s = swat.CAS('localhost', 5570, authinfo=AUTHINFO)
+            s.loadactionset('image')
+            s.loadimages(filepath, casout={'name':'img'})
+            s.loadactionset('astore')
+            s.score(table={'name':'img'}, out={'name':'score'}, rstore={'name':ASTORE, 'caslib':ASTORE_LIB})
+            scores = s.fetch(table={'name':'score'})['Fetch'].loc[0,:].to_dict()
+            label = scores.pop('I__label_')
+            s.endSession()
+            return jsonify({'imgUrl': url_for('uploaded_file', filename=filename),
+                'label': label,
+                'scores': scores})
 
-	return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 def allowed_file(filename):
-	return '.' in filename and \
-		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
-	app.run(debug=True, host=APP_IP, port=APP_PORT)
+    app.run(debug=True, host=APP_IP, port=APP_PORT)
--- static/js/script.js
+++ static/js/script.js
@@ -2,73 +2,73 @@ $("#image-results").hide();
 $("#exit").hide();
 
 function turnIntoArray(dictionary) {
-	var array = [];
-	$.each(dictionary, function(index, value) {
-		array.push({classification: index, probability: value});
-	});
-	return array;
+  var array = [];
+  $.each(dictionary, function(index, value) {
+    array.push({classification: index, probability: value});
+  });
+  return array;
 }
 
 function sortScores(scores) {
-	return scores.sort(function(a, b) {
-		return b.probability - a.probability;
-	});
+  return scores.sort(function(a, b) {
+    return b.probability - a.probability;
+  });
 }
 
 Dropzone.autoDiscover = false;
 
 $(function () {
-	var myDropzone = new Dropzone("#my-awesome-dropzone");
-	myDropzone.on("success", function(file, response) {
-		console.log(response);
-		$("#image-demo").hide();
-		$("#image-preview img").attr("src", response.imgUrl);
+  var myDropzone = new Dropzone("#my-awesome-dropzone");
+  myDropzone.on("success", function(file, response) {
+    console.log(response);
+    $("#image-demo").hide();
+    $("#image-preview img").attr("src", response.imgUrl);
 
-		var scores = turnIntoArray(response.scores);
-		scores = sortScores(scores);
-		console.log(scores);
+    var scores = turnIntoArray(response.scores);
+    scores = sortScores(scores);
+    console.log(scores);
 
-		var scoreDivs = [];
-		$.each(scores, function(index, score) {
-			console.log(score);
-			var classification = score.classification.slice(9);
-			
-			var row = $(document.createElement("div"));
-			row.addClass("ty-row ty-labels");
+    var scoreDivs = [];
+    $.each(scores, function(index, score) {
+      console.log(score);
+      var classification = score.classification.slice(9);
+      
+      var row = $(document.createElement("div"));
+      row.addClass("ty-row ty-labels");
 
-			var name = $(document.createElement("div"));
-			name.addClass("name ty-labels");
-			name.text(classification);
+      var name = $(document.createElement("div"));
+      name.addClass("name ty-labels");
+      name.text(classification);
 
-			var prob = $(document.createElement("div"));
-			prob.addClass("score ty-labels");
-			prob.text(Math.round(score.probability * 100) + "%");
+      var prob = $(document.createElement("div"));
+      prob.addClass("score ty-labels");
+      prob.text(Math.round(score.probability * 100) + "%");
 
-			var bar = $(document.createElement("div"));
-			bar.addClass("bar ty-labels");
+      var bar = $(document.createElement("div"));
+      bar.addClass("bar ty-labels");
 
-			var meter = $(document.createElement("div"));
-			meter.addClass("meter ty-labels");
-			meter.css("width", prob.text());
+      var meter = $(document.createElement("div"));
+      meter.addClass("meter ty-labels");
+      meter.css("width", prob.text());
 
-			bar.append(meter);
-			row.append(name, prob, bar);
+      bar.append(meter);
+      row.append(name, prob, bar);
 
-			scoreDivs.push(row);
-		});
+      scoreDivs.push(row);
+    });
 
-		$("#score-card").append(scoreDivs);
-		$("#image-results").show();
-		$("#exit").show();
-	});
+    $("#score-card").append(scoreDivs);
+    $("#image-results").show();
+    $("#exit").show();
+  });
 
-	$("#exit").click(function() {
-		$("#image-preview img").attr("src", "");
-		$("#score-card").empty();
-		$("#image-results").hide();
-		myDropzone.removeAllFiles();
-		$("#image-demo").show();
-		$("#exit").hide();
-	});
+  $("#exit").click(function() {
+    $("#image-preview img").attr("src", "");
+    $("#score-card").empty();
+    $("#image-results").hide();
+    myDropzone.removeAllFiles();
+    $("#image-demo").show();
+    $("#exit").hide();
+  });
 });
 
