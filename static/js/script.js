b6b54d28113954ad3b07abdfc19be63cc82fa498
Tyler Freckmann <tyler.freckmann@sas.com>
2018-06-01 20:40:54 -0400

    add close button to image recognition results

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 $("#image-results").hide();
+$("#exit").hide();
 
 function turnIntoArray(dictionary) {
 	var array = [];
@@ -14,49 +15,60 @@ function sortScores(scores) {
 	});
 }
 
-Dropzone.options.myAwesomeDropzone = {
-	init: function() {
-		this.on("success", function(file, response) {
-			console.log(response);
-			$("#image-demo").hide();
-			$("#image-preview img").attr("src", response.imgUrl);
+Dropzone.autoDiscover = false;
 
-			var scores = turnIntoArray(response.scores);
-			scores = sortScores(scores);
-			console.log(scores);
+$(function () {
+	var myDropzone = new Dropzone("#my-awesome-dropzone");
+	myDropzone.on("success", function(file, response) {
+		console.log(response);
+		$("#image-demo").hide();
+		$("#image-preview img").attr("src", response.imgUrl);
 
-			var scoreDivs = [];
-			$.each(scores, function(index, score) {
-				console.log(score);
-				var classification = score.classification.slice(9);
-				
-				var row = $(document.createElement("div"));
-				row.addClass("ty-row ty-labels");
+		var scores = turnIntoArray(response.scores);
+		scores = sortScores(scores);
+		console.log(scores);
 
-				var name = $(document.createElement("div"));
-				name.addClass("name ty-labels");
-				name.text(classification);
+		var scoreDivs = [];
+		$.each(scores, function(index, score) {
+			console.log(score);
+			var classification = score.classification.slice(9);
+			
+			var row = $(document.createElement("div"));
+			row.addClass("ty-row ty-labels");
 
-				var prob = $(document.createElement("div"));
-				prob.addClass("score ty-labels");
-				prob.text(Math.round(score.probability * 100) + "%");
+			var name = $(document.createElement("div"));
+			name.addClass("name ty-labels");
+			name.text(classification);
 
-				var bar = $(document.createElement("div"));
-				bar.addClass("bar ty-labels");
+			var prob = $(document.createElement("div"));
+			prob.addClass("score ty-labels");
+			prob.text(Math.round(score.probability * 100) + "%");
 
-				var meter = $(document.createElement("div"));
-				meter.addClass("meter ty-labels");
-				meter.css("width", prob.text());
+			var bar = $(document.createElement("div"));
+			bar.addClass("bar ty-labels");
 
-				bar.append(meter);
-				row.append(name, prob, bar);
+			var meter = $(document.createElement("div"));
+			meter.addClass("meter ty-labels");
+			meter.css("width", prob.text());
 
-				scoreDivs.push(row);
-			});
-			$("#score-card").append(scoreDivs);
-			$("#image-results").show();
+			bar.append(meter);
+			row.append(name, prob, bar);
+
+			scoreDivs.push(row);
 		});
-	}
-};
 
+		$("#score-card").append(scoreDivs);
+		$("#image-results").show();
+		$("#exit").show();
+	});
+
+	$("#exit").click(function() {
+		$("#image-preview img").attr("src", "");
+		$("#score-card").empty();
+		$("#image-results").hide();
+		myDropzone.removeAllFiles();
+		$("#image-demo").show();
+		$("#exit").hide();
+	});
+});
 
